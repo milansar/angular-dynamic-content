c1e08598a37b2f23fa59dbdb80921783a4b5c927
Milan Saraiya <milan@ashutec.com>
2019-02-24 00:26:08 +0530

    changed structure so dynamically loaded are components are not part of main bundle saving intial download

--- src/app/dynamic-multiple/dynamic-multiple.module.ts
+++ src/app/dynamic-multiple/dynamic-multiple.module.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { NgModule, Type } from '@angular/core';
+import {EntryComponentsMap } from './../common/types';
 import { DynamicMultipleOneComponent } from './dynamic-multiple-one.component';
 import { DynamicMultipleTwoComponent } from './dynamic-multiple-two.component';
+
+const entryComponentsMap: EntryComponentsMap = {
+  DynamicMultipleOneComponent,
+  DynamicMultipleTwoComponent
+};
 
 @NgModule({
   declarations: [DynamicMultipleOneComponent, DynamicMultipleTwoComponent],
   imports: [CommonModule],
   entryComponents: [DynamicMultipleOneComponent, DynamicMultipleTwoComponent]
 })
-export class DynamicMultipleModule {}
+export class DynamicMultipleModule {
+  static entryComponentsMap: EntryComponentsMap = entryComponentsMap;
+}
